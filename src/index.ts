710fae6164c93293a311ce88eb48c9e56e99f113
glebassty <glebassty@gmail.com>
2025-08-20 19:36:30 +0300

    refactor index ts

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,21 @@
 import express from "express";
+import http from "http";
 import { sequelize } from "./config/db";
+import stickerRoutes from "./routes/stickerRoutes";
+import { initSocket } from "./services/socketService";
 import boardRoutes from "./routes/boardRoutes";
 
 const app = express();
+const server = http.createServer(app);
+
+initSocket(server);
+
 app.use(express.json());
-
-app.use("/boards", boardRoutes);
-
+app.use("/boards", stickerRoutes);
+app.use("/boards", boardRoutes); 
 const PORT = process.env.PORT || 4000;
 
 (async () => {
   await sequelize.sync({ alter: true });
-  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 })();
-
