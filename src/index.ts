08601056e5c4dc7ec597236433c1feb5f1ac5820
glebassty <glebassty@gmail.com>
2025-08-20 18:12:21 +0300

    add sequelize sync on server start

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,17 @@
 import express from "express";
+import { sequelize } from "./config/db";
 
 const app = express();
 app.use(express.json());
 
+(async () => {
+  await sequelize.sync();
+})();
+
 const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+(async () => {
+  await sequelize.sync({ alter: true });
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+})();
+
